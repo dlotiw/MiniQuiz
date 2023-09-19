3f33822ea29d4d57b54129a91c27cffde64ee47a
Witold Brzykcy <witold.brzykcy@ukw.edu.pl>
2023-09-19 11:26:44 +0200

    Updated the files to work with new gui

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import requests
-from question_model import Question
-from data import Root
-from quiz_brain import QuizBrain
+from data_model import Root
+from quiz import Quiz
+from ui import QuizUI
 
+def main():
+    parameters = {
+        'amount': 10,
+        'type': 'boolean',
+    }
+    trivia = requests.get("https://opentdb.com/api.php?",params=parameters)
+    dane_tr = Root.from_dict(trivia.json())
 
-parameters = {
-    'amount': 10,
-    'type': 'boolean',
-}
-trivia = requests.get("https://opentdb.com/api.php?",params=parameters)
+    quiz = Quiz(dane_tr.results)
+    screen = QuizUI(question=quiz)
 
+    screen.mainloop()
 
-dane_tr = Root.from_dict(trivia.json())
-
-quiz = QuizBrain(dane_tr.results)
-
-while quiz.still_has_questions():
-    quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+if __name__ == '__main__':
+    main()
--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from quiz import Quiz
 
 THEME_COLOR = "#375362"
@@ -6,18 +7,18 @@ THEME_COLOR = "#375362"
 class QuizUI:
     
     def __init__(self, question: Quiz) -> 'QuizUI':
-        self.score = 0
+        self.question = question
         self.window = Tk()    
         self.window.title("MiniQuiz")
         self.window.config(padx=20,pady=20,background=THEME_COLOR)
         #Score
-        self.score_label = Label(text=f"Score: {self.score}")
+        self.score_label = Label(text=f"Score: {self.question.score}/{len(self.question.question_list)}")
         self.score_label.config(padx=20,pady=20,fg="white",bg=THEME_COLOR,font=("Arial",20,"bold"))
         self.score_label.grid(row=0,column=1)
 
         #QuizQuestion
         self.canvas = Canvas(width=400,height=250,background='white',highlightthickness=0,)
-        self.question = self.canvas.create_text(200,125,
+        self.question_text = self.canvas.create_text(200,125,
                                                 text=question.current_question.question,fill=THEME_COLOR,
                                                 font=("Ariel",20,"italic"),width=380)
         self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
@@ -36,13 +37,29 @@ class QuizUI:
         
 
     def add_score(self):
-        return self.score + 1
+        self.score_label.config(text=f"Score: {self.question.score}/{len(self.question.question_list)}")
+        
 
     def wrong_button_click(self):
-        return False
+        if self.question.check_answer('False'):
+            self.add_score()
+        if self.question.next_question():
+            self.canvas.itemconfig(self.question_text,text=self.question.current_question.question)
+        else:
+            self.ending()
+        
     
     def right_button_click(self):
-        return True
+        if self.question.check_answer('True'):
+            self.add_score()
+        if self.question.next_question():
+            self.canvas.itemconfig(self.question_text,text=self.question.current_question.question)
+        else:
+            self.ending()
     
+    def ending(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.window.destroy()
+
     def mainloop(self):
         self.window.mainloop()
